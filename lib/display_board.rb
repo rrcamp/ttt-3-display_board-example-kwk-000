1c08422b7bb058da4feeaf4cfcc49ea1d3b74f74
Rose Ryan Campau <rcampau@icloud.com>
2016-06-23 00:23:30 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+row = "   |   |   "
+sep = "-----------"
+
+
+def display_board
+  puts "   |   |   ",
+       "-----------",
+       "   |   |   ",
+       "-----------",
+       "   |   |   "
+end
